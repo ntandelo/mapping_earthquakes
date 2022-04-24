8333034723090dd4f69e5ce9a9ca34c0b14e02ad
Nicole Tandel <nicoletandel@Nicoles-MacBook-Air.local>
2022-04-24 17:04:40 -0600

    new branch push

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -8,8 +8,15 @@ let map = L.map("mapid", {
     ],
     zoom: 4
   });
+
+  let marker = L.circleMarker([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: '#ffcf00',
+    fillOpacity: 0.5,
+    radius: 300
+ }).addTo(map);
   // We create the tile layer that will be the background of our map.
-  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         accessToken: API_KEY
